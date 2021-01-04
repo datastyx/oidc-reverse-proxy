264a9d5721599d7d1fe05e2912d0f0124130681f
Dominic Lecuyer <dominic.lecuyer@reach.nato.int>
2021-01-04 12:44:09 +0100

    rolling back wrong fix

--- script/launch.sh
+++ script/launch.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo "### Run OIDC Reverse Proxy"
-envsubst "${DNS_RESOLVER},${PORT},${REDIRECT_URI},${DISCOVERY_URL},${CLIENT_ID},${CLIENT_SECRET},${REDIRECT_URI_SCHEME},${LOGOUT_PATH},${AFTER_LOGOUT_URI},${BACKEND_HOST}" < /config/nginx.tmpl.conf > /config/nginx.conf 
+envsubst '${DNS_RESOLVER},${PORT},${REDIRECT_URI},${DISCOVERY_URL},${CLIENT_ID},${CLIENT_SECRET},${REDIRECT_URI_SCHEME},${LOGOUT_PATH},${AFTER_LOGOUT_URI},${BACKEND_HOST}' < /config/nginx.tmpl.conf > /config/nginx.conf 
 /usr/local/openresty/nginx/sbin/nginx -g "daemon off;" -c /config/nginx.conf
